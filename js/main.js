2eac1a3b1e2e30b336e1b86cc110ccb355f363f1
shirquille.sande <shirquille.sande@student.moringaschool.com>
2021-06-22 09:32:50 +0300

    Resources added

--- js/main.js
+++
@@ -1,9 +0,0 @@
-
-    var name = $("input#MERGE1").val();
-    var email = $("input#MERGE0").val();
-    var message = $("textarea#comment").val();
-    if ($("input#MERGE1").val() && $("input#MERGE0").val()){
-      alert (name + ", we have received your message. Thank you for reaching out to us.");
-    }
-    else {   
-      alert("Please enter your name and email!")}  
